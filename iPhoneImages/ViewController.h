ee17b9f91ec8699bc8fadeff675bca14ad29402a
Suziewan <angielinton@me.com>
2017-01-30 11:54:20 -0500

    Initial Commit

---
+++ iPhoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneImages
+//
+//  Created by Angie Linton on 2017-01-30.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPhoneImages/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhoneImages
+//
+//  Created by Angie Linton on 2017-01-30.
+//  Copyright © 2017 Angie Linton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
